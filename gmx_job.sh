6deeb98414c7ec0195939ffca0cc62b7d871ab85
Viswajit <35959160+ViswajitMulpuru@users.noreply.github.com>
2023-06-16 14:56:28 +0530

    gmx_pbs
    shell script to submit GROMACS job via PBS

--- gmx_job.sh
+++ gmx_job.sh
@@ -8,8 +8,10 @@
 cd /home/nidhi/rec
 source /apps/gromacs/gpu/bin/GMXRC
 source /apps/intel/parallel_studio_xe_2018_update3_cluster_edition/bin/compilervars.sh intel64
+
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 cat $PBS_NODEFILE | uniq > nodes
 nvidia-smi | tee nv.log
+
 cd PBS_O_WORKDIR
 mpirun -np 2 /apps/gromacs/gpu/bin/gmx_mpi mdrun -ntomp 20 -pin on -cpi -deffnm md
